372d98241a6ef130dde88bb4ab5039a8d7a20050
Piotr Balcer <piotr.balcer@intel.com>
2024-11-04 11:06:45 +0100

    improve benchmark scripts and add a nightly job
    This patch:
    - Adds a benchmark suite abstraction and moves all existing benchmarks
      into it. This makes individual benchmark types self-contained and
      allows us to skip over benchmarks that don't have their dependencies
      met.
    - Makes sycl and ur optional, and they are no longer positional
      arguments.
    - Creates a benchmark history that stores benchmark runs. This then
      enables us to do comparisions not just with the latest result but
      also against e.g., a historical average.
    - adds a nightly job to store baseline results.
    - adds HTML output

--- scripts/benchmarks/benches/options.py
+++ scripts/benchmarks/benches/options.py
@@ -1,13 +1,23 @@
 from dataclasses import dataclass
+from enum import Enum
+
+class Compare(Enum):
+    LATEST = 'latest'
+    AVERAGE = 'average'
+    MEDIAN = 'median'
 
 @dataclass
 class Options:
-    sycl: str = ""
+    sycl: str = None
+    ur: str = None
+    ur_adapter: str = None
     rebuild: bool = True
     benchmark_cwd: str = "INVALID"
     timeout: float = 600
     iterations: int = 5
     verbose: bool = False
+    compare: Compare = Compare.LATEST
+    compare_max: int = 10 # average/median over how many results
 
 options = Options()
 
